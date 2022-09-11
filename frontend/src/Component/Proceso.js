562df571f7a10547cc041cbc05e47ed98d34c49f
Abrahamn <abraham81423@gmail.com>
2022-09-11 12:58:00 -0600

    [ADD] Process

--- frontend/src/Component/Proceso.js
+++ frontend/src/Component/Proceso.js
@@ -1,12 +1,24 @@
-import './css/Proceso.css'
+// import M from '@materializecss/materialize/dist/js/materialize.min';
+import './css/Proceso.css';
+import Process from './Process/Process';
 
 function Proceso(props){
 
 	return(
 		<div className="proceso">
 			<center>
-				<h3>Proceso</h3>
+				<h1>Proceso</h1>
+				<br />
+				<div className="row">
+				</div>
+				<div className="row">
+					<Process />
+				</div>
+				<br />
+				<h1>Sub-Proceso</h1>
+				<br />
 			</center>
+				
 		</div>
 	);
 }
