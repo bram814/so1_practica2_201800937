5f4a1d3b688791789d6f094129f02b133ae2cc03
Abrahamn <abraham81423@gmail.com>
2022-09-11 01:02:28 -0600

    [ADD] script DDL y DML

---
+++ script/DDL.sql
@@ -0,0 +1,11 @@
+-- ESTUDIANTE EJEMPLO
+insert into ESTUDIANTE(name, carnet) VALUES('Jose Abraham Solorzano Herrera','201800937');
+SELECT * FROM ESTUDIANTE;
+
+-- RAM EJEMPLO
+INSERT INTO RAM (total, free, used) VALUES (100,100,100);
+SELECT * FROM RAM;
+
+-- CPU EJEMPLO
+INSERT INTO CPU (pid, name, state, user, ram, parent) VALUES (26, 'INE', 1026,'0',120,0);
+SELECT * FROM CPU;
